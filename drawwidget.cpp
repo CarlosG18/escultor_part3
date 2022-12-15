1f3d5593acd92a758cef344ae7a0564b8ca1bcaf
CarlosG18 <c.gabriel.abc18@gmail.com>
2022-12-15 20:25:00 -0300

    add loadMatrix

--- drawwidget.cpp
+++ drawwidget.cpp
@@ -34,8 +34,8 @@ void DrawWidget::paintEvent(QPaintEvent *event)
   
   for(int i=0; i<nlinhas; i++){
     for(int j=0; j<ncolunas; j++){
-      if(v[i][j][planoZ].isOn == true){
-        brush.setColor(QColor(v[i][j][planoZ].r,v[i][j][planoZ].g,v[i][j][planoZ].b));
+      if(v[i][j].isOn == true){
+        brush.setColor(QColor(v[i][j].r,v[i][j].g,v[i][j].b));
 		    //painter.drawEllipse(QPoint(i*(width()/n)+(width()/2*n),j*(height()/m)+(height()/2*m),10,10));
       }else{
         brush.setColor(QColor(255,255,255));
@@ -52,7 +52,21 @@ void DrawWidget::mousePressEvent(QMouseEvent *event){
 }
 
 void DrawWidget::loadMatrix(std::vector<std::vector<Voxel>> l){
-  
+
+  int i,j;
+  std::vector<Voxel> linha;
+
+  for(i=0; i<nlinhas; i++){
+    linha.resize(ncolunas);
+    for(j=0; j<ncolunas; j++){
+      linha[j].r = l[i][j].r;
+      linha[j].g = l[i][j].g;
+      linha[j].b = l[i][j].b;
+      linha[j].a = l[i][j].a;
+      linha[j].isOn = l[i][j].isOn;
+    }
+    v.push_back(linha);
+  }
 }
 
 void DrawWidget::setTamanho(int nlinhas, int ncolunas){
